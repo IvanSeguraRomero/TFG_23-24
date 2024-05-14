3cfce3086a415b42b388fa4f1344f00db9278002
GGSZGZ <guillermo.garcia@fundacionmontessori.com>
2024-05-14 17:40:27 +0200

    Fonts&Styles

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,60 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HeaderItem from './components/HeaderItem.vue';
-import FooterItem from './components/FooterItem.vue';
-//import css
-// import './assets/main.css'
+import HeaderItem from './components/HeaderItem.vue'
+import FooterItem from './components/FooterItem.vue'
+import './assets/main.css'
 </script>
 
 <template>
-  <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&display=swap">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lobster:wght@400&display=swap">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Gentium+Basic:wght@400;700&display=swap">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Onest:wght@700&display=swap"> -->
+  <link
+    href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap"
+    rel="stylesheet"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Orbitron:wght@400..900&display=swap"
+    rel="stylesheet"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Orbitron:wght@400..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+    rel="stylesheet"
+  />
   <header>
     <HeaderItem>
-      <template #admin><RouterLink to="/admin" style="text-decoration: none; color: var(--neutral-colors-headings-black)">Admin</RouterLink></template>
-      <template #home><RouterLink to="/" style="text-decoration: none; color: var(--neutral-colors-headings-black)">Store</RouterLink></template>
-      <template #about><RouterLink to="/library" style="text-decoration: none; color: var(--neutral-colors-headings-black)" >Library</RouterLink></template>
-      <template #contact><RouterLink to="/aboutUs" style="text-decoration: none; color: var(--neutral-colors-headings-black)" >About us</RouterLink></template>
-      <template #profile><RouterLink to="/login" style="text-decoration: none; color: var(--neutral-colors-headings-black)" >Login</RouterLink></template>
+      <template #admin
+        ><RouterLink
+          to="/admin"
+          style="text-decoration: none; color: var(--neutral-colors-headings-black)"
+          >Admin</RouterLink
+        ></template
+      >
+      <template #home
+        ><RouterLink
+          to="/"
+          style="text-decoration: none; color: var(--neutral-colors-headings-black)"
+          >Store</RouterLink
+        ></template
+      >
+      <template #about
+        ><RouterLink
+          to="/library"
+          style="text-decoration: none; color: var(--neutral-colors-headings-black)"
+          >Library</RouterLink
+        ></template
+      >
+      <template #contact
+        ><RouterLink
+          to="/aboutUs"
+          style="text-decoration: none; color: var(--neutral-colors-headings-black)"
+          >About us</RouterLink
+        ></template
+      >
+      <template #profile
+        ><RouterLink
+          to="/login"
+          style="text-decoration: none; color: var(--neutral-colors-headings-black)"
+          >Login</RouterLink
+        ></template
+      >
     </HeaderItem>
   </header>
 
@@ -27,14 +63,14 @@ import FooterItem from './components/FooterItem.vue';
       <RouterView />
     </div>
   </main>
-  
+
   <footer>
     <FooterItem>
       <template #hrindex></template>
       <!--Redes Sociales-->
-      <template #facebook><img alt="" src="./assets/facebook.svg" ></template>
-      <template #instagram><img alt="" src="./assets/instagram.svg"></template>
-      <template #twitter><img alt="" src="./assets/twitter.svg"></template>
+      <template #facebook><img alt="" src="./assets/facebook.svg" /></template>
+      <template #instagram><img alt="" src="./assets/instagram.svg" /></template>
+      <template #twitter><img alt="" src="./assets/twitter.svg" /></template>
     </FooterItem>
   </footer>
 </template>
@@ -48,13 +84,13 @@ header {
   background-color: --neutral-colors-white;
   width: 100%;
   height: 110px;
-  background-color: #D8DC06;
+  background-color: #d8dc06;
 }
 footer {
   background-color: --neutral-colors-white;
   width: 100%;
   height: 110px;
-  background-color: #D8DC06;
+  background-color: #d8dc06;
 }
 .logosolo-texto-1 {
   position: absolute;
