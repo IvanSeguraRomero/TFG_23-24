e6991218fd057b899775e5b16e0edb48f73a2efd
GGSZGZ <guillermo.garcia@fundacionmontessori.com>
2024-05-14 19:24:11 +0200

    Fetch users done

--- src/store/api.ts
+++ src/store/api.ts
@@ -6,8 +6,7 @@ const pinia = createPinia()
 // Exportamos la tienda
 export const useApiStore = defineStore('flashgaminghub', {
   state: () => ({
-    // loggedInUser: JSON.parse(localStorage.getItem('user')!),
-    // admin: false
+    token: localStorage.getItem('jwtToken') || null,
   }),
   actions: {
     async fetchPostLoginUser(user: any) {
@@ -28,14 +27,165 @@ export const useApiStore = defineStore('flashgaminghub', {
         const data = await response.text()
         const token = data
 
-        // localStorage.setItem('jwtToken', token)
+        return token
+      } catch (error) {
+        console.error('Error al enviar los datos:', error)
+        return null
+      }
+    },
+    async fetchPostRegisterUser(user: any) {
+      try {
+        const response = await fetch('https://localhost:7025/Auth/Register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(user)
+        })
+
+        if (!response.ok) {
+          console.error(`Error: ${response.status} - ${response.statusText}`)
+          return null
+        }
+
+        const data = await response.text()
+        const token = data
 
         return token
       } catch (error) {
         console.error('Error al enviar los datos:', error)
         return null
       }
+    },
+    //USER
+    async fetchUsers() {
+      try {
+
+        if (!this.token) {
+          console.error('No se encontró ningún token JWT en el localStorage')
+          return
+        }
+
+        const response = await fetch('https://localhost:7025/User', {
+          headers: {
+            Authorization: `Bearer ${this.token}`
+          }
+        })
+        if (!response.ok) {
+          throw new Error('Error al obtener los datos')
+        }
+        return await response.json()
+      } catch (error: any) {
+        console.error('Error al obtener los datos:', error.message)
+        throw error
+      }
+    },
+    async fetchUser(idUser: number) {
+      try {
+
+        if (!this.token) {
+          console.error('No se encontró ningún token JWT en el localStorage')
+          return
+        }
+
+        const response = await fetch(`https://localhost:7025/User/${idUser}`, {
+          headers: {
+            Authorization: `Bearer ${this.token}`
+          }
+        })
+        if (!response.ok) {
+          throw new Error('Error al obtener los datos')
+        }
+        return await response.json()
+      } catch (error: any) {
+        console.error('Error al obtener los datos:', error.message)
+        throw error
+      }
+    },
+    async fetchMessagesUser(idUser: number) {
+      try {
+
+        if (!this.token) {
+          console.error('No se encontró ningún token JWT en el localStorage')
+          return
+        }
+
+        const response = await fetch(`https://localhost:7025/User/${idUser}/messages`, {
+          headers: {
+            Authorization: `Bearer ${this.token}`
+          }
+        })
+        if (!response.ok) {
+          throw new Error('Error al obtener los datos')
+        }
+        return await response.json()
+      } catch (error: any) {
+        console.error('Error al obtener los datos:', error.message)
+        throw error
+      }
+    },
+    async fetchDeleteUser(idUser: number) {
+      try {
+
+        if (!this.token) {
+          console.error('No se encontró ningún token JWT en el localStorage')
+          return
+        }
+
+        const response = await fetch(`https://localhost:7025/User/${idUser}`, {
+          method: 'DELETE',
+          headers: {
+            Authorization: `Bearer ${this.token}`
+          }
+        })
+        if (!response.ok) {
+          throw new Error('Error al eliminar el usuario')
+        }
+        return 'Usuario eliminado correctamente'
+      } catch (error: any) {
+        console.error('Error al eliminar el usuario:', error.message)
+        throw error
+      }
+    },
+    async fetchUpdateUser(idUser: number, userData: any) {
+      
+      try {
+        if (!this.token) {
+          console.error('No se encontró ningún token JWT en el localStorage');
+          return;
+        }
+
+        const formattedUserData: { [key: string]: any } = {};
+        Object.keys(userData).forEach(key => {
+          formattedUserData[key] = userData[key];
+        });
+        const response = await fetch(`https://localhost:7025/User/${idUser}`, {
+          method: 'PUT',
+          headers: {
+            Authorization: `Bearer ${this.token}`,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formattedUserData)
+        });
+    
+        if (!response.ok) {
+          throw new Error('Error al actualizar el usuario')
+          
+        }
+        const responseBody = await response.text();
+        if (!responseBody) {
+          return null;
+        }
+    
+        return JSON.parse(responseBody);
+      } catch (error: any) {
+        
+        console.error('Error al actualizar el usuario:', error.message);
+        throw error;
+      }
     }
+    //END USER
+    
   }
 })
 
--- src/views/StoreView.vue
+++ src/views/StoreView.vue
@@ -1,28 +1,48 @@
 <script setup lang="ts">
 import { useApiStore, pinia } from '../store/api'
-import { ref, onMounted } from 'vue'
-import { useRouter } from 'vue-router'
 import { jwtDecode } from 'jwt-decode'
 
-//import css
-
 const user = {
-  email: 'user1@example.com',
-  password: 'password1'
+  email: 'user2@example.com',
+  password: 'password2'
 }
-
+// const userRegister = {
+//   name: 'Probandoregister',
+//   surname: 'Desde frontend',
+//   password: '1234',
+//   age: 21,
+//   email: 'hola@registrado.com',
+//   registerDate: '2024-05-14T15:47:16.200Z',
+//   active: true,
+//   role: 'admin'
+// }
+//put user prueba
+const userData = {
+  age: 777,
+};
+// test login
 const loginUser = async () => {
   const token = await useApiStore(pinia).fetchPostLoginUser(user)
   if (token) {
     localStorage.setItem('jwtToken', token)
-    console.log('Token recibido:', token)
-    loginUserToken()
+    decodedToken()
   } else {
     console.error('Error al iniciar sesión.')
   }
 }
+//test register
+// const registerUser = async () => {
+//   const token = await useApiStore(pinia).fetchPostRegisterUser(userRegister)
+//   if (token) {
+//     localStorage.setItem('jwtToken', token)
+//     console.log('Token recibido:', token)
+//     decodedToken()
+//   } else {
+//     console.error('Error al iniciar sesión.')
+//   }
+// }
 
-const loginUserToken = async () => {
+const decodedToken = async () => {
   const token = localStorage.getItem('jwtToken')
 
   if (!token) {
@@ -33,21 +53,23 @@ const loginUserToken = async () => {
   // Decodificar el token JWT
   const decodedToken = jwtDecode(token)
 
-  console.log('Token decodificado:', decodedToken)
+  // console.log('Token decodificado:', decodedToken)
+  console.log('Token sin decodificar:', token)
+  await useApiStore(pinia).fetchUpdateUser(decodedToken.id,userData)
+  // console.log(put)
 }
-
-// Llamar a la función para decodificar el token
 loginUser()
+// registerUser()
 </script>
 
 <template>
   <main>
-  <h1>HOLAStore</h1>
+    <h1>HOLAStore</h1>
   </main>
 </template>
 
 <style scoped>
-  main{
+main {
   background-color: var(--color-maroon);
   width: 100%;
   height: auto;
@@ -57,4 +79,3 @@ loginUser()
   font-family: var(--font-lobster);
 }
 </style>
-
